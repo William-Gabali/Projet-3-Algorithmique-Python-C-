49ca9269061cb9f33023e74fbf6d187f2ba6734a
William-Gabali <117296703+William-Gabali@users.noreply.github.com>
2022-11-18 17:40:44 +0100

    Create PFC_game pseudo code.py

---
+++ PFC_game pseudo code.py
@@ -0,0 +1,85 @@
+# definit la fonction random qui genere une variable aleatoire pour l'IA
+
+# Crée un Tableau avec des options 
+
+# Attribuer une option aléatoire à l'IA
+
+# affiché les points du Joueur 1
+
+# affiché les points de l'IA
+
+# Assignez continuer a Vrai
+
+# La boucle continue jusqu'à ce que la variable continuer est fausse
+
+    # affiche le message "Vous Choisisez :"
+    
+    # assigner jun aux retour de la fonction input qui va retourné le message :
+    # pierre, papier, ciseaux? ou tapez FIN pour arrêter le jeu!\n"
+    
+    # Si Jun est egale a FIN
+    
+        # Alors Continuer est egale a Faux et met fin a la partie 
+        
+    # Sinon SI Jun est egale a Bots
+    
+        #Alors
+        
+    #Sinon SI Jun est egale a pierre
+    
+        #SI Bots est egale a papier
+        
+            #Alors affiché le message "Tu a Perdu!", Bots, "recouvre", jun
+        
+            #Atribuer 1 point a l'IA
+        
+        #Sinon 
+        
+            #Alors affiché le message "Tu a Gagné!", jun, "écrase", Bots
+        
+            #Atribuer 1 point au joueurs 
+    
+    #Sinon SI jun est egale a papier
+    
+        #SI Bots est egale a ciseaux 
+    
+            #Alors Aficher le message "Perdu!", Bots, "coupe", jun
+    
+            #Atribuer 1 point a l'IA
+    
+        #Sinon
+    
+            #Alors afficher le message "Tu a Gagné!", jun, "recouvre", Bots
+    
+            #Atribuer 1 point au joueurs 
+    
+    #Sinon SI jun est egale a ciseaux
+    
+        #Si Bots est egale a pierre
+    
+            #Alors afficher le message "Tu a Perdu!", Bots, "écrase", jun
+    
+            #Atribuer 1 point a l'IA
+    
+        #Sinon
+    
+            #Alors afficher le message "Tu a Gagné!", jun, "coupe", Bots
+    
+            #Atribuer 1 point au joueurs 
+    
+    #Sinon
+    
+        #Alors Afficher un message d'erreur " tu t'est trompé"
+    
+    #Attribuer une option aléatoire à l'IA
+    
+    #affiché un message "tour suivant"
+
+#Afficher les points
+
+# affiché un message "Points"
+
+# affiché un message ""J1: ", Pointsjun"
+
+# affiché un message "IA: ", PointsBots"
+
